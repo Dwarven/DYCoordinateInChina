9a8120fcc7c3bd1f98d294ad05b14bd7375ddaa4
Dwarven YANG <prison.yang@gmail.com>
2016-02-29 14:55:47 +0800

    add files

---
+++ DYCoordinateInChina.h
@@ -0,0 +1,19 @@
+//
+//  DYCoordinateInChina.h
+//  https://github.com/Dwarven/DYCoordinateInChina
+//
+//  Copyright (c) 2016 Dwarven Yang https://github.com/Dwarven
+//
+//  Created by Dwarven on 16/2/29.
+//  Copyright © 2016 Dwarven Yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DYCoordinateInChina : NSObject
+
++ (instancetype)sharedInstance;
+
+- (BOOL)coordinateInChina:(CLLocationCoordinate2D)coordinate;
+
+@end
